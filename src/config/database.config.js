d074fc615f046f7f949776a079fb42ec90f68337
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-12 10:54:56 +0700

    update db config

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected success: ${conn.connection.host}`);
   } catch (error) {
-    console.error("MongoDB connected error");
+    console.error("MongoDB connected error: ", error.message);
     // 1 - error
     // 0 - succses
     // process of node
